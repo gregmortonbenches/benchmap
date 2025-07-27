e4294af8985948a9e70e939d67130a1cbf289fe4
gregmortonbenches <greg_morton@Hotmail.co.uk>
2025-07-27 15:41:50 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import React from 'react'
-import MapView from './components/MapView'
-import SearchBar from './components/SearchBar'
+import BenchMap from './components/BenchMap'
 
-export default function App() {
+function App() {
   return (
-    <div className="flex flex-col h-screen">
-      <header className="bg-blue-600 text-white p-4 text-xl font-bold">
-        Bench Map
-      </header>
-      <SearchBar />
-      <MapView />
+    <div>
+      <BenchMap />
     </div>
   )
 }
+
+export default App
