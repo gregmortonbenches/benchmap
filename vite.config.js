dca934a6a89e2bfca274c30908d790bbcfbda872
gregmortonbenches <greg_morton@Hotmail.co.uk>
2025-07-28 22:57:11 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/benchmap/', // 👈 this is critical for GitHub Pages!
+  base: '/benchmap/',
+  optimizeDeps: {
+    include: ['leaflet.heat'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['leaflet.heat'],
+    }
+  }
 })
