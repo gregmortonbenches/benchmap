57685ac852bba651c88ea53750239d04dbe66219
gregmortonbenches <greg_morton@Hotmail.co.uk>
2025-07-29 20:53:18 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import commonjs from '@rollup/plugin-commonjs'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    commonjs(), // ✅ ensures leaflet.heat is bundled correctly
-  ],
-  base: '/benchmap/', // ✅ must match your GitHub repo name
-  optimizeDeps: {
-    include: ['leaflet.heat'],
-  },
+  plugins: [react()],
+  base: '/benchmap/',
 })
