1576fa38d5e8bdb23e0aaf90ae4a9afb96aadd38
gregmortonbenches <greg_morton@Hotmail.co.uk>
2025-07-29 20:40:13 +0100

    Update BenchMap.jsx

--- src/components/BenchMap.jsx
+++ src/components/BenchMap.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import L from 'leaflet';
 import 'leaflet.heat';               // <-- Import leaflet.heat like this, no relative path
 import 'leaflet/dist/leaflet.css';
